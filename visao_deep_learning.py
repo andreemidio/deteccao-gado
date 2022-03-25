58c31ed5586a2f5863b48237289ed62cd0e41d35
Andre Emidio <andresjc2008@gmail.com>
2022-03-24 21:10:36 -0300

    Visão Deep learning

---
+++ visao_deep_learning.py
@@ -0,0 +1,127 @@
+import cv2
+import numpy as np
+
+confThreshold = 0.5  
+nmsThreshold = 0.4   
+inpWidth = 416       
+inpHeight = 416      
+
+        
+# Load names of classes
+classesFile = "modelos/coco.names"
+classes = None
+with open(classesFile, 'rt') as f:
+    classes = f.read().rstrip('\n').split('\n')
+
+modelConfiguration = "modelos/yolov3-tiny.cfg"
+modelWeights = "modelos/yolov3-tiny.weights"
+
+net = cv2.dnn.readNetFromDarknet(modelConfiguration, modelWeights)
+
+net.setPreferableBackend(cv2.dnn.DNN_BACKEND_OPENCV)
+net.setPreferableTarget(cv2.dnn.DNN_TARGET_OPENCL)
+print('Using GPU device.')
+
+def getOutputsNames(net):
+
+    layersNames = net.getLayerNames()
+
+    return [layersNames[i-1] for i in net.getUnconnectedOutLayers()]
+
+def drawPred(classId, conf, left, top, right, bottom):
+    cv2.rectangle(frame, (left, top), (right, bottom), (255, 178, 50), 3)
+    
+    label = '%.2f' % conf
+        
+    if classes:
+        assert(classId < len(classes))
+        label = '%s:%s' % (classes[classId], label)
+
+    labelSize, baseLine = cv2.getTextSize(label, cv2.FONT_HERSHEY_SIMPLEX, 0.5, 1)
+    top = max(top, labelSize[1])
+    cv2.rectangle(frame, (left, top - round(1.5*labelSize[1])), (left + round(1.5*labelSize[0]), top + baseLine), (255, 255, 255), cv2.FILLED)
+    cv2.putText(frame, label, (left, top), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0,0,0), 1)
+
+def postprocess(frame, outs):
+    frameHeight = frame.shape[0]
+    frameWidth = frame.shape[1]
+
+
+    classIds = []
+    confidences = []
+    boxes = []
+    for out in outs:
+        for detection in out:
+            scores = detection[5:]
+            classId = np.argmax(scores)
+            confidence = scores[classId]
+            if confidence > confThreshold:
+                center_x = int(detection[0] * frameWidth)
+                center_y = int(detection[1] * frameHeight)
+                width = int(detection[2] * frameWidth)
+                height = int(detection[3] * frameHeight)
+                left = int(center_x - width / 2)
+                top = int(center_y - height / 2)
+                classIds.append(classId)
+                confidences.append(float(confidence))
+                boxes.append([left, top, width, height])
+
+
+    indices = cv2.dnn.NMSBoxes(boxes, confidences, confThreshold, nmsThreshold)
+    for i in indices:
+        box = boxes[i]
+        left = box[0]
+        top = box[1]
+        width = box[2]
+        height = box[3]
+        drawPred(classIds[i], confidences[i], left, top, left + width, top + height)
+
+
+winName = 'Detecção de Gado'
+cv2.namedWindow(winName, cv2.WINDOW_NORMAL)
+
+
+
+print("Input video file doesn't exist")
+
+cap = cv2.VideoCapture("gado.mp4")
+
+
+outputFile = 'gado.mp4_yolo_novo_out_py.avi'
+
+
+vid_writer = cv2.VideoWriter(outputFile, cv2.VideoWriter_fourcc('M','J','P','G'), 30, (round(cap.get(cv2.CAP_PROP_FRAME_WIDTH)),round(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))))
+
+while cv2.waitKey(1) < 0:
+    
+
+    hasFrame, frame = cap.read()
+    
+
+    if not hasFrame:
+        print("Done processing !!!")
+        print("Output file is stored as ", outputFile)
+        cv2.waitKey(3000)
+
+        cap.release()
+        break
+
+    blob = cv2.dnn.blobFromImage(frame, 1/255, (inpWidth, inpHeight), [0,0,0], 1, crop=False)
+
+    net.setInput(blob)
+
+    outs = net.forward(getOutputsNames(net))
+
+    postprocess(frame, outs)
+
+    t, _ = net.getPerfProfile()
+    label = 'Inference time: %.2f ms' % (t * 1000.0 / cv2.getTickFrequency())
+    cv2.putText(frame, label, (0, 15), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255))
+
+
+    vid_writer.write(frame.astype(np.uint8))
+
+    cv2.imshow(winName, frame)
+
+cv2.destroyAllWindows()
+exit()
